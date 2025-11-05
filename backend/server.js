4a9d5ea373c110616323a2159a6d399cf9a0fc51
Tyllen Aziz <tyllenaziz@gmail.com>
2025-11-05 18:43:06 +0300

    Adding patching Method

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
